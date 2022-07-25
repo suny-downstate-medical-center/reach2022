8386b1e44119d191bb13e7ad8a1be33d89e50564
craig <kelley92@bu.edu>
2022-07-25 16:36:58 -0400

    add pops

--- netParams.py
+++ netParams.py
@@ -8,3 +8,7 @@ h.load_file('stdrun.hoc')
 netParams.importCellParams(label='PYR', fileName='geom.py', cellName='PyrAdr', cellArgs={'x' : 0, 'y' : 0, 'z' : 0, 'id' : 0})
 netParams.importCellParams(label='OLM', fileName='geom.py', cellName='Ow', cellArgs={'x' : 0, 'y' : 0, 'z' : 0, 'id' : 0})
 netParams.importCellParams(label='PV', fileName='geom.py', cellName='PVC', cellArgs={'x' : 0, 'y' : 0, 'z' : 0, 'id' : 0})
+
+netParams.popParams['PYR'] = {'cellType' : 'PYR', 'numCells' : 1}
+netParams.popParams['OLM'] = {'cellType' : 'OLM', 'numCells' : 1}
+netParams.popParams['PV'] + {'cellType' : 'PV', 'numCells' : 1}
